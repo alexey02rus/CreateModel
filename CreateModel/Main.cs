94db4dedbb24f4f79d6bce5df64307c49f3e11e5
alexey02rus <101921891+alexey02rus@users.noreply.github.com>
2022-12-04 23:32:23 +0300

    Part3

--- CreateModel/Main.cs
+++ CreateModel/Main.cs
@@ -23,6 +23,7 @@ namespace CreateModel
 
             double width = 10000;
             double depth = 5000;
+            double roofHight = 1500;
 
             List<Wall> walls = CreateWallsToRectangle(doc, width, depth, level1, level2, false);
             if (walls == null)
@@ -61,6 +62,12 @@ namespace CreateModel
                 }
             }
 
+            ExtrusionRoof newRoof = CreateExtrusionRoof(doc, width, depth, roofHight, level2);
+            if (newRoof == null)
+            {
+                TaskDialog.Show("Ошибка", "Не удалось создать крышу");
+                return Result.Failed;
+            }
 
             return Result.Succeeded;
         }
@@ -82,10 +89,12 @@ namespace CreateModel
                 TaskDialog.Show("Внимание!", "Указаны слишком маленькие значения ширины или глубины");
                 return null;
             }
+            WallType defaultTypeWall = document.GetElement(document.GetDefaultElementTypeId(ElementTypeGroup.WallType)) as WallType;
+            double offset = defaultTypeWall.Width / 2.0;
             double widthToUnits = UnitUtils.ConvertToInternalUnits(width, UnitTypeId.Millimeters);
             double depthToUnits = UnitUtils.ConvertToInternalUnits(depth, UnitTypeId.Millimeters);
-            double dx = widthToUnits / 2.0;
-            double dy = depthToUnits / 2.0;
+            double dx = widthToUnits / 2.0 - offset;
+            double dy = depthToUnits / 2.0 - offset;
             List<XYZ> points = new List<XYZ>()
             {
                 new XYZ(-dx, -dy, 0),
@@ -102,6 +111,7 @@ namespace CreateModel
                     Line line = Line.CreateBound(points[i], points[i + 1]);
                     Wall wall = Wall.Create(document, line, baseLevel.Id, isStructural);
                     wall.get_Parameter(BuiltInParameter.WALL_HEIGHT_TYPE).Set(upLevel.Id);
+                    wall.get_Parameter(BuiltInParameter.WALL_KEY_REF_PARAM).Set(2);
                     walls.Add(wall);
                 }
                 ts.Commit();
@@ -156,6 +166,45 @@ namespace CreateModel
         {
             Doors = BuiltInCategory.OST_Doors,
             Windows = BuiltInCategory.OST_Windows
+        }
+
+        public ExtrusionRoof CreateExtrusionRoof(Document document,
+                                                 double width,
+                                                 double depth,
+                                                 double hight,
+                                                 Level baseLevel)
+        {
+            RoofType roofType = new FilteredElementCollector(document)
+                .OfClass(typeof(RoofType))
+                .OfType<RoofType>()
+                .Where(r => r.Name.Equals("Типовой - 400мм"))
+                .FirstOrDefault();
+            if (roofType == null || baseLevel == null)
+            {
+                return null;
+            }
+            double offset = UnitUtils.ConvertToInternalUnits(200, UnitTypeId.Millimeters); ;
+            double dY = UnitUtils.ConvertToInternalUnits(depth / 2.0, UnitTypeId.Millimeters);
+            double dX = UnitUtils.ConvertToInternalUnits(width / 2.0, UnitTypeId.Millimeters);
+            double dZ = baseLevel.Elevation + offset;
+            double dH = dZ + UnitUtils.ConvertToInternalUnits(hight, UnitTypeId.Millimeters);
+
+            CurveArray sketchRoof = new CurveArray();
+            sketchRoof.Append(Line.CreateBound(new XYZ(0, dY, dZ), new XYZ(0, 0, dH)));
+            sketchRoof.Append(Line.CreateBound(new XYZ(0, 0, dH), new XYZ(0, -dY, dZ)));
+
+            ExtrusionRoof extrusionRoof;
+            using (Transaction ts = new Transaction(document))
+            {
+                ts.Start("Создание крыши");
+                ReferencePlane plane = document.Create.NewReferencePlane(new XYZ(0, 0, 0), new XYZ(0, 0, 1), new XYZ(0, 1, 0), document.ActiveView);
+                extrusionRoof = document.Create.NewExtrusionRoof(sketchRoof, plane, baseLevel, roofType, -dX, dX);
+                extrusionRoof.get_Parameter(BuiltInParameter.ROOF_CONSTRAINT_OFFSET_PARAM).Set(UnitUtils.ConvertToInternalUnits(offset, UnitTypeId.Millimeters));
+                extrusionRoof.get_Parameter(BuiltInParameter.ROOF_EAVE_CUT_PARAM).Set(33619);
+                extrusionRoof.get_Parameter(BuiltInParameter.FASCIA_DEPTH_PARAM).Set(offset);
+                ts.Commit();
+            }
+            return extrusionRoof;
         }
     }
 }
